08a13ccd25ae640dd988248c20a36f524a506620
laracarter22 <ucnvlnc@ucl.ac.uk>
2024-12-10 16:51:49 +0000

    adding extra bits for finetuning

--- named-entity-recognition/requirements.py
+++ named-entity-recognition/requirements.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 
+#I need transformers, datasets, seqeval, 
+
 # Install the transformers package
 try:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "transformers"])
@@ -8,4 +10,14 @@ try:
 except subprocess.CalledProcessError as e:
     print(f"An error occurred while installing 'transformers': {e}")
 
+try:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "datasets"])
+    print("Successfully installed 'datasets'!")
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred while installing 'datasets': {e}")
 
+try:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "seqeval"])
+    print("Successfully installed 'seqeval'!")
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred while installing 'seqeval': {e}")
